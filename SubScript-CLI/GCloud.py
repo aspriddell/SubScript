c686ca89d7a03fe330253add0fcc4c8bd81cb153
Albie Spriddell <albion@dragonfruit.ml>
2019-05-03 17:45:48 +0100

    edit the backend and properly test the translator

--- SubScript-CLI/GCloud.py
+++ SubScript-CLI/GCloud.py
@@ -1,19 +1,23 @@
 import os
+import json
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-def IsCredFileSet():
-	try:
-		if os.environ['GOOGLE_APPLICATION_CREDENTIALS'] != "":
-			return True
-		else:
-			raise Exception
-	except:
-		return False
+envVar = 'GOOGLE_APPLICATION_CREDENTIALS'
 
+def IsCredFileSet():
+	if os.getenv(envVar) is not None:
+		return True
+	else:
+		return False
 
 def FindAndSetCred():
 	Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 	filename = askopenfilename(title = "Select Google JSON",filetypes = [("Google Credential Files","*.json")]) # show an "Open" dialog box and return the path to the selected file
 	if filename != "":
-		os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = filename
+		os.environ[envVar] = filename
+
+def GetProjectName():
+	with open(os.environ[envVar], 'r') as file:
+		jsonObj = json.loads(file.read().replace('\n', ''))
+		return str(jsonObj["project_id"])
--- SubScript-CLI/SubScript_CLI.py
+++ SubScript-CLI/SubScript_CLI.py
@@ -1,5 +1,24 @@
 import os
 import GCloud
+from google.cloud import translate_v3beta1 as translate
 
 if GCloud.IsCredFileSet() == False:
 	GCloud.FindAndSetCred()
+
+client = translate.TranslationServiceClient()
+
+project_id = GCloud.GetProjectName()
+text = 'こんにちは'
+location = 'global'
+
+parent = client.location_path(project_id, location)
+
+response = client.translate_text(
+    parent=parent,
+    contents=[text],
+    mime_type='text/plain',  # mime types: text/plain, text/html
+    source_language_code='ja-JP',
+    target_language_code='en-US')
+
+for translation in response.translations:
+    print('Translated Text: {}'.format(translation))
