2fca1ac6ec5a7ecbaebc61041fdaccc82b157449
Albie Spriddell <albion@dragonfruit.network>
2019-05-04 07:06:44 +0100

    better splitting method

--- SubScript-CLI/SSABridge.py
+++ SubScript-CLI/SSABridge.py
@@ -2,16 +2,17 @@ import pysubs2
 import GUI
 import re
 
+reg = '(?i)(\\\\n)|(\\n)|({.*})' #regex to isolate line breaks and the stage directions \\N and {} (insensitive)
+
 def Load():
 	file = GUI.LoadSubs();
 	if file != "":
 		subs = pysubs2.load(file, encoding="utf-8") #load subs file
 		for a in subs.events:
-			listofparts = str(a.text).split('\\N') #split newline
-			editedlist = []
-			for x in range(0,len(listofparts)):
-				editedlist.append(listofparts[x]) #add to new list
-				if x < len(listofparts)-1: #ignore the last part
-					editedlist.append('\n') #add newline char
-			editedlist = list(filter(None, editedlist))
-			print("O: "+a.text+" | E: "+str(editedlist)) #filter [''] entries
+			editedlist = list(filter(None, re.split(reg,a.text))) 
+			tt = []
+			for i in range(0,len(editedlist)):
+				if not re.match(reg,str(editedlist[i])):
+					tt.append(i)
+
+			print("L: "+str(editedlist) + "T: " + str(tt)) #filter [''] entries
